e92ccd744280a977145bfed9eb155a92a0b30387
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-12-20 17:42:17 +0100

    Modification 3

--- src/main/java/pl/zajavka/case1/Cat.java
+++ src/main/java/pl/zajavka/case1/Cat.java
@@ -2,6 +2,12 @@ package pl.zajavka.case1;
 
 public class Cat extends Animal {
 
+    private String milk;
+
+    public Cat(String milk) {
+        this.milk = milk;
+    }
+
     @Override
     public String whatIsYourName() {
         return "My name is Cat";
--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -3,8 +3,7 @@ package pl.zajavka.case1;
 public class Dog extends Animal {
     private String name;
 
-    @Override
-    public String whatIsYourName() {
-        return "My name is Doggo";
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
     }
 }
