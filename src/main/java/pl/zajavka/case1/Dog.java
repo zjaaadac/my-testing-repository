99d65655b13e8ca33b1c7acf9fec62b125c9d727
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-12-20 14:06:42 +0100

    Modification 2

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,5 +1,9 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-    private String name;
+
+    @Override
+    public String whatIsYourName() {
+        return "My name is Dog";
+    }
 }
