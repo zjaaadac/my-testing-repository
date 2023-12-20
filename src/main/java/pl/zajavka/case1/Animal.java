e35356cf426e05be5189462cdc7fc6be15e7531a
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-12-20 15:06:43 +0100

    Revert "Modification 2"
    This reverts commit e92c6ab8

--- src/main/java/pl/zajavka/case1/Animal.java
+++ src/main/java/pl/zajavka/case1/Animal.java
@@ -2,8 +2,6 @@ package pl.zajavka.case1;
 
 public abstract class Animal {
 
-    private String name;
-
     protected String whatIsYourName() {
         return "I don't have any";
     }
--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -2,8 +2,4 @@ package pl.zajavka.case1;
 
 public class Dog extends Animal {
     private String name;
-
-    public void sniff() {
-        System.out.println("I'm soo sniffing!");
-    }
 }
